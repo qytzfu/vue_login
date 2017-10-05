2350b484a558cf57758eac28e5e50a262fa514c1
qytzfu <qytzfu@gmail.com>
2017-10-06 00:53:42 +0800

    添加了add功能,修复了table没有行线

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,52 +3,49 @@
       <form action="" role="form">
           <div class="form-group">
               <label for="username">用户名:</label>
-              <input type="text" id="username" class="form-control" placeholder="输入用户名">
+              <input type="text" id="username" class="form-control" placeholder="输入用户名" v-model="username">
           </div>
           <div class="form-group">
               <label for="age">年龄:</label>
-              <input type="text" id="age" class="form-control" placeholder="输入年龄">
+              <input type="text" id="age" class="form-control" placeholder="输入年龄" v-model="age">
           </div>
           <div class="form-group">
-              <input type="button" value="添加" class="btn btn-primary">
+              <input type="button" value="添加" class="btn btn-primary" v-on:click="add()">
               <input type="button" value="重置" class="btn btn-danger">
           </div>
       </form>
       <hr>
       <table class="table table-hover">
-        <caption class="h2 text-info">用户信息表</caption>
-        <tr class="text-danger">
-          <th class="text-center">序号</th>
-          <th class="text-center">名字</th>
-          <th class="text-center">年龄</th>
-          <th class="text-center">操作</th>
-        </tr>
-        <tr class="text-center">
-            <td>1</td>
-            <td>blue</td>
-            <td>50</td>
+        <caption class="h2 text-info text-center">用户信息表</caption>
+        <thead>
+          <tr class="text-danger">
+            <th class="text-center">序号</th>
+            <th class="text-center">名字</th>
+            <th class="text-center">年龄</th>
+            <th class="text-center">操作</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- v-bind:key 这里绑定name不好,并不唯一,暂时先这样,后可以绑定index? -->
+          <tr class="text-center" v-for="(item,index) in profile" v-bind:key="index">
+            <td>{{index+1}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.age}}</td>
             <td>
-                <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#layer">删除</button>
+                  <button class="btn btn-primary btn-sm" data-toggle="modal" data-target="#layer">删除</button>
             </td>
-        </tr>
-        <tr class="text-center">
-            <td>1</td>
-            <td>strive</td>
-            <td>16</td>
-            <td>
-                <button class="btn btn-primary btn-sm">删除</button>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="4" class="text-right">
-                <button class="btn btn-danger btn-sm">删除全部</button>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="4" class="text-center text-muted">
-                <p>暂无数据....</p>
-            </td>
-        </tr>
+          </tr>
+          <tr v-show="profile.length!=0">
+              <td colspan="4" class="text-right">
+                  <button class="btn btn-danger btn-sm">删除全部</button>
+              </td>
+          </tr>
+          <tr v-show="profile.length==0">
+              <td colspan="4" class="text-center text-muted">
+                  <p>暂无数据....</p>
+              </td>
+          </tr>
+        </tbody>
       </table>
       <!-- 弹出框 -->
       <div role="dialog" class="modal fade" id="layer">
@@ -78,7 +75,24 @@ export default {
   name: 'hello',
   data () {
     return {
+      profile:[],
+      username:'',
+      age:'',
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    add:function(){
+      this.profile.push({
+        name:this.username,
+        age:this.age
+      });
+      console.log(this.username);
+      console.log(this.age);
+      this.username='';
+      this.age='';
+      // alert(this.profile)
+      console.log(this.profile);
     }
   }
 }
